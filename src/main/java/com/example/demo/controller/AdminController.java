2418149794fe601c4065f1b88653db1e6a9539f2
yeonggyo <dldudry2222@gmail.com>
2025-07-04 15:45:02 +0900

    오후까지 한거 커밋

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -1,5 +1,57 @@
 package com.example.demo.controller;
 
-public class AdminController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-}
+import com.example.demo.service.AdminService;
+
+import jakarta.servlet.http.HttpSession;
+
+	@Controller
+	@RequestMapping("/admin")
+	public class AdminController {
+
+	    @Autowired
+	    private AdminService adminService;
+
+	    // 관리자 로그인폼 이동
+	    @GetMapping("/adminLogin")
+	    public String loginPage() {
+	        return "admin/adminLogin";
+	    }
+
+	    // 관리자 로그인
+	    @PostMapping("/login")
+	    public String login(@RequestParam("id") String id,
+	                        @RequestParam("password") String password,
+	                        HttpSession session,
+	                        Model model) {
+	        if (adminService.login(id, password)) {
+	            System.out.println("✅ 로그인 성공: " + id);
+	            session.setAttribute("admin", id);
+	            return "redirect:/admin/adminHome";
+	        }
+	        model.addAttribute("loginError", "아이디 또는 비밀번호가 올바르지 않습니다.");
+	        return "admin/adminLogin";
+	    }
+	    
+	    // 로그아웃
+	    @GetMapping("/logout")
+	    public String logout(HttpSession session) {
+	        session.invalidate(); // 세션 전체 삭제 (로그아웃 처리)
+	        return "redirect:/admin/adminLogin";
+	    }
+	    
+	    // 홈화면 이동
+	    @GetMapping("/adminHome")
+	    public String home() {
+			 System.out.println("✅ 홈 화면 진입");
+	        return "admin/adminHome"; // home.jsp로 이동
+	    }
+		
+	}
---
+++ src/main/webapp/WEB-INF/views/admin/adminHome.jsp
@@ -0,0 +1,35 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+
+<html>
+<head>
+  <title>관리자 홈</title>
+  <link rel="stylesheet" href="<c:url value='/css/adminHome.css' />">
+</head>
+<body>
+
+  <h1>관리자 홈 화면</h1>
+
+  <div class="menu-container">
+    <div class="menu-box">
+      <a href="/admin/adminMember">회원관리</a>
+    </div>
+    <div class="menu-box">
+      <a href="/admin/adminStats">통계</a>
+    </div>
+    <div class="menu-box">
+      <a href="/admin/adminReservation">예약내역</a>
+    </div>
+    <%-- <div class="menu-box">
+       <%@ include file="/WEB-INF/views/game/gamedataMaster.jsp" %> 
+    </div> --%>
+     <div class="menu-box">
+    <a href="/admin/logout">로그아웃</a>
+    </div>
+  </div>
+
+</body>
+</html>
